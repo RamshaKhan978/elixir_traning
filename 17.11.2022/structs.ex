03573c1fa4d120c4ea0733430e5787c9cfbf969c
RamshaKhan978 <118250485+RamshaKhan978@users.noreply.github.com>
2022-11-17 18:39:18 +0500

    .

--- 17.11.2022/structs.ex
+++ 17.11.2022/structs.ex
@@ -1,14 +1,29 @@
 defmodule Person do
-  defstruct[
+  defstruct [
     first_name: nil,
     last_name: nil,
     birthday: nil,
     location: "home"
   ]
-  def home ( %Person {} = person ) do
-    %{person | locaton: "home"}
+  def full_name(%Person{first_name: first_name,
+  last_name: last_name} = person) do
+    "#{first_name} #{last_name}"
   end
-    def away ( %Person {} = person ) do
-      %{person | locaton: "away"}
-    end
+
+  def age(%Person{birthday: birthday} = person) do
+    days = Date.diff(Date.utc_today, birthday)
+    days / 365.25
+  end
+
+
+  def home(%Person{} = person) do
+    %{person | location: "home"}
+  end
+  def home(%Person{} = person) do
+    %{person | location: "home"}
+  end
+
+  def away(%Person{} = person) do
+    %{person | location: "away"}
+  end
       end
