c3e990de90b726f85f83cdaaf783a25002c6ad96
RamshaKhan978 <118250485+RamshaKhan978@users.noreply.github.com>
2022-11-18 18:11:52 +0500

    test

---
+++ people/test/person_test.exs
@@ -0,0 +1,18 @@
+defmodule People.PersonTest do
+  use ExUnit.Case
+  alias People.Person
+
+  test "full_name/1 with mononyms" do
+    teller = %Person{
+      first_name: "Teller"
+    }
+
+    assert teller |> Person.full_name() === "Teller"
+
+    madonna = %Person{
+      first_name: "Madonna"
+    }
+
+    assert madonna |> Person.full_name() === "Madonna"
+  end
+end
