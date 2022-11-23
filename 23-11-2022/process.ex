0f15e8f4dfb4be6411aa766f80768dc252e5ed11
RamshaKhan978 <118250485+RamshaKhan978@users.noreply.github.com>
2022-11-23 22:28:37 +0500

    state

--- 23-11-2022/process.ex
+++ 23-11-2022/process.ex
@@ -1,10 +1,22 @@
 defmodule  Parallel do
-  def listen do
-    receive do
-      {:ok, "hello"} -> IO.puts("World")
-    end
-
-
+  def pmap(collection ,fun) do
+    collection
+    |> Enum.map(&spawn_process(&1, self, fun))
+    |> Enum.map(&await/1)
   end
 
+
+   defp spawn_process(item, parent, fun) do
+     spawn_link fn ->
+      send parent, {self, fun.(item)}
+     end
+   end
+
+   defp await(pid) do
+      receive do
+        {^pid, result} -> result
+          # code
+      end
+
+   end
 end
---
+++ 23-11-2022/state.ex
@@ -0,0 +1,29 @@
+defmodule KV do
+  def start_link do
+    Task.start_link(fn -> loop(%{}) end)
+  end
+
+  defp loop(map) do
+    receive do
+      {:get, key, caller} ->defmodule KV do
+  def start_link do
+    Task.start_link(fn -> loop(%{}) end)
+  end
+
+  defp loop(map) do
+    receive do
+      {:get, key, caller} ->
+        send caller, Map.get(map, key)
+        loop(map)
+      {:put, key, value} ->
+        loop(Map.put(map, key, value))
+    end
+  end
+end
+        send caller, Map.get(map, key)
+        loop(map)
+      {:put, key, value} ->
+        loop(Map.put(map, key, value))
+    end
+  end
+end
