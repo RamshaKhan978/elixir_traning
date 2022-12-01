c07dd6216120e208a7624b5459ddf24a130a102e
RamshaKhan978 <118250485+RamshaKhan978@users.noreply.github.com>
2022-12-01 22:05:40 +0500

    1-12-2022

--- 30-11-2022/kv_umbrella/apps/kv_server/lib/kv_server/application.ex
+++ 30-11-2022/kv_umbrella/apps/kv_server/lib/kv_server/application.ex
@@ -7,10 +7,11 @@ defmodule KVServer.Application do
 
   @impl true
   def start(_type, _args) do
+    port = String.to_integer(System.get_env("PORT") || "4040") |> IO.inspect
     children = [
-      port = String.to_integer(System.get_env("PORT") || "4040")
-
-      {Task, fn -> KVServer.accept(port) end}
+      {Task.Supervisor, name: KVServer.TaskSupervisor},
+      Supervisor.child_spec({Task, fn -> KVServer.accept(port) end},
+      restart: :permanent)
     ]
 
     opts = [strategy: :one_for_one, name: KVServer.Supervisor]
--- 30-11-2022/kv_umbrella/apps/kv_server/test/kv_server_test.exs
+++ 30-11-2022/kv_umbrella/apps/kv_server/test/kv_server_test.exs
@@ -2,7 +2,4 @@ defmodule KVServerTest do
   use ExUnit.Case
   doctest KVServer
 
-  test "greets the world" do
-    assert KVServer.hello() == :world
-  end
 end
